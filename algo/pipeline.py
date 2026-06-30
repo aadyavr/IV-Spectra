227e919d1b881092e20397fa8b0fde6de967f194
aadyavr <aadyavr25@iitk.ac.in>
2026-06-30 11:04:15 +0530

    Implement Pipeline class with process method

---
+++ algo/pipeline.py
@@ -0,0 +1,23 @@
+class Pipeline:
+
+    def __init__(self):
+
+        pass
+
+    def process(
+
+        self,
+
+        voltage,
+
+        current
+
+    ):
+
+        result = {}
+
+        result["voltage"] = voltage
+
+        result["current"] = current
+
+        return result
